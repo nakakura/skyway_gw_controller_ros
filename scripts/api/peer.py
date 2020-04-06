3e71bd07902385ba7fcdff8c53a61a3872840fe4
Toshiya Nakakura <nakakura@me.com>
2020-04-06 12:15:10 +0900

    add POST /peer API

--- scripts/api/peer.py
+++ scripts/api/peer.py
@@ -11,7 +11,53 @@ class PeerId:
         self.name = "peer_id"
 
 
+class ApiResponse:
+    def __init__(self, data, err):
+        # type: (dict, str) -> None
+        """docstring here"""
+
+        self.data = data
+        self.err = err
+
+    def is_ok(self):
+        # type: () -> bool
+        """docstring here"""
+        return self.err == ""
+
+
+def post(url, payload, expected_code):
+    # type: (str, dict, int) -> ApiResponse
+    """docstring here"""
+
+    headers = {"content-type": "application/json"}
+    resp = requests.post(url, headers=headers, json=payload, timeout=(3.0, 1.0))
+
+    if resp.status_code == expected_code:
+        response = ApiResponse(resp.json, "")
+    elif resp.status_code == 400:
+        response = ApiResponse({}, url + "returns 400 Bad Request")
+    elif resp.status_code == 403:
+        response = ApiResponse({}, url + "returns 403 Forbidden")
+    elif resp.status_code == 404:
+        response = ApiResponse({}, url + "returns 404 Not Found")
+    elif resp.status_code == 405:
+        response = ApiResponse({}, url + "returns 405 Method Not Allowed")
+    elif resp.status_code == 406:
+        response = ApiResponse({}, url + "returns 406 Not Acceptable")
+    elif resp.status_code == 408:
+        response = ApiResponse({}, url + "returns 408 Request Timeout")
+    else:
+        response = ApiResponse({}, url + "returns Unexpected Status Code")
+
+    return response
+
+
+# This method call POST /peer API to create PeerObject
+# http://35.200.46.204/#/1.peers/peer
 def create_peer(url, key, domain, peer_id, turn):
+    # type: (str, str, str, PeerId, bool) -> ApiResponse
+    """docstring here"""
+
     if (
         isinstance(url, str)
         and isinstance(key, str)
@@ -21,19 +67,5 @@ def create_peer(url, key, domain, peer_id, turn):
     ):
         rospy.logerr("isinstance true")
 
-    headers = {"content-type": "application/json"}
     payload = {"key": key, "domain": domain, "peer_id": peer_id, "turn": turn}
-    resp = requests.post(
-        url + "/peers", headers=headers, json=payload, timeout=(3.0, 1.0)
-    )
-    rospy.logerr("hoge")
-    rospy.logerr(resp.json())
-
-    if resp.status_code == 201:
-        print(json.dumps(resp.json(), indent=2))
-    elif resp.status_code == 202:
-        print("hoge")
-    else:
-        print("hoge")
-
-    return resp.json()
+    return post(url + "/peers", payload, 201)
