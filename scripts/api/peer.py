d922e22af44efdd69b4ab7113ab9fe635754c1ee
Toshiya Nakakura <nakakura@me.com>
2020-04-07 10:41:07 +0900

    add test for create_peer

--- scripts/api/peer.py
+++ scripts/api/peer.py
@@ -7,22 +7,29 @@ from typing import NewType
 
 
 class PeerId:
-    def __init__(self):
-        self.name = "peer_id"
+    def __init__(self, peer_id):
+        # type: (str) -> PeerId
+        self.peer_id = peer_id
+
+    def id(self):
+        # type: () -> str
+        return self.peer_id
 
 
 class ApiResponse:
     def __init__(self, data, err):
-        # type: (dict, str) -> None
-        """docstring here"""
+        # type: (dict, dict) -> None
 
         self.data = data
         self.err = err
 
     def is_ok(self):
         # type: () -> bool
-        """docstring here"""
-        return self.err == ""
+        return self.err == {}
+
+    def json(self):
+        # type: () -> dict
+        return self.data
 
 
 def post(url, payload, expected_code):
@@ -30,26 +37,31 @@ def post(url, payload, expected_code):
     """docstring here"""
 
     headers = {"content-type": "application/json"}
-    resp = requests.post(url, headers=headers, json=payload, timeout=(3.0, 1.0))
+    try:
+        resp = requests.post(url, headers=headers, json=payload, timeout=(3.0, 1.0))
 
-    if resp.status_code == expected_code:
-        response = ApiResponse(resp.json, "")
-    elif resp.status_code == 400:
-        response = ApiResponse({}, url + "returns 400 Bad Request")
-    elif resp.status_code == 403:
-        response = ApiResponse({}, url + "returns 403 Forbidden")
-    elif resp.status_code == 404:
-        response = ApiResponse({}, url + "returns 404 Not Found")
-    elif resp.status_code == 405:
-        response = ApiResponse({}, url + "returns 405 Method Not Allowed")
-    elif resp.status_code == 406:
-        response = ApiResponse({}, url + "returns 406 Not Acceptable")
-    elif resp.status_code == 408:
-        response = ApiResponse({}, url + "returns 408 Request Timeout")
-    else:
-        response = ApiResponse({}, url + "returns Unexpected Status Code")
+        if resp.status_code == expected_code:
+            response = ApiResponse(resp.json(), {})
+        elif resp.status_code == 400:
+            response = ApiResponse({}, {"url": url, "error": "400 Bad Request"})
+        elif resp.status_code == 403:
+            response = ApiResponse({}, {"url": url, "error": "403 Forbidden"})
+        elif resp.status_code == 404:
+            response = ApiResponse({}, {"url": url, "error": "404 Not Found"})
+        elif resp.status_code == 405:
+            response = ApiResponse({}, {"url": url, "error": "405 Method Not Allowed"})
+        elif resp.status_code == 406:
+            response = ApiResponse({}, {"url": url, "error": "406 Not Acceptable"})
+        elif resp.status_code == 408:
+            response = ApiResponse({}, {"url": url, "error": "408 Request Timeout"})
+        else:
+            response = ApiResponse({}, {"url": url, "error": "Unexpected Status Code"})
 
-    return response
+        return response
+    except requests.exceptions.RequestException as e:
+        return ApiResponse({}, e)
+    except Exception as e:
+        raise e
 
 
 # This method call POST /peer API to create PeerObject
@@ -67,5 +79,5 @@ def create_peer(url, key, domain, peer_id, turn):
     ):
         rospy.logerr("isinstance true")
 
-    payload = {"key": key, "domain": domain, "peer_id": peer_id, "turn": turn}
+    payload = {"key": key, "domain": domain, "peer_id": peer_id.id(), "turn": turn}
     return post(url + "/peers", payload, 201)
--- test/api/peer.py
+++ test/api/peer.py
@@ -1,24 +1,177 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 PKG = "skyway"
+import logging
+import rospy
 import roslib
 
 roslib.load_manifest(PKG)
 
 import sys
 import unittest
+import mock
+from mock import patch
 from os import path
+import requests
 
 sys.path.append(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
 from scripts.api.peer import *
 
 
-class TestPeer(unittest.TestCase):
-    def test_one_equals_one(self):  # only functions with 'test_'-prefix will be run!
-        self.assertEquals(1, 1, "1!=1")
+def mocked_requests_post(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+    if args[0] == "url_201":
+        if kwargs["json"] == {"param": "valid"}:
+            return MockResponse({"key1": "value1"}, 201)
+        else:
+            return MockResponse({}, 403)
+    elif args[0] == "url_400":
+        return MockResponse({}, 400)
+    elif args[0] == "url_403":
+        return MockResponse({}, 403)
+    elif args[0] == "url_404":
+        return MockResponse({}, 404)
+    elif args[0] == "url_405":
+        return MockResponse({}, 405)
+    elif args[0] == "url_406":
+        return MockResponse({}, 406)
+    elif args[0] == "url_408":
+        return MockResponse({}, 408)
+    elif args[0] == "url_create_peer_success/peers":
+        return MockResponse(kwargs["json"], 201)
+    elif args[0] == "url_create_peer_fail/peers":
+        return MockResponse({}, 404)
+
+    return MockResponse({}, 410)
+
+
+class TestPostResp(unittest.TestCase):
+
+    # success case
+    @mock.patch("requests.post", side_effect=mocked_requests_post)
+    def test_post_success(self, mock_post):
+        response = post("url_201", {"param": "valid"}, 201)
+        self.assertEqual(response.json(), {"key1": "value1"})
+        self.assertEqual(response.is_ok(), True)
+
+    # return 400
+    @mock.patch("requests.post", side_effect=mocked_requests_post)
+    def test_post_400(self, mock_post):
+        response = post("url_400", {"param": "valid"}, 201)
+        self.assertEqual(response.err, {"url": "url_400", "error": "400 Bad Request"})
+        self.assertEqual(response.is_ok(), False)
+
+    # return 403
+    @mock.patch("requests.post", side_effect=mocked_requests_post)
+    def test_post_403(self, mock_post):
+        response = post("url_403", {"param": "valid"}, 201)
+        self.assertEqual(response.err, {"url": "url_403", "error": "403 Forbidden"})
+        self.assertEqual(response.is_ok(), False)
+
+    # return 404
+    @mock.patch("requests.post", side_effect=mocked_requests_post)
+    def test_post_404(self, mock_post):
+        response = post("url_404", {"param": "valid"}, 201)
+        self.assertEqual(response.err, {"url": "url_404", "error": "404 Not Found"})
+        self.assertEqual(response.is_ok(), False)
+
+    # return 405
+    @mock.patch("requests.post", side_effect=mocked_requests_post)
+    def test_post_405(self, mock_post):
+        response = post("url_405", {"param": "valid"}, 201)
+        self.assertEqual(
+            response.err, {"url": "url_405", "error": "405 Method Not Allowed"}
+        )
+        self.assertEqual(response.is_ok(), False)
+
+    # return 406
+    @mock.patch("requests.post", side_effect=mocked_requests_post)
+    def test_post_406(self, mock_post):
+        response = post("url_406", {"param": "valid"}, 201)
+        self.assertEqual(
+            response.err, {"url": "url_406", "error": "406 Not Acceptable"}
+        )
+        self.assertEqual(response.is_ok(), False)
+
+    # return 408
+    @mock.patch("requests.post", side_effect=mocked_requests_post)
+    def test_post_408(self, mock_post):
+        response = post("url_408", {"param": "valid"}, 201)
+        self.assertEqual(
+            response.err, {"url": "url_408", "error": "408 Request Timeout"}
+        )
+        self.assertEqual(response.is_ok(), False)
+
+    # return unknown code
+    @mock.patch("requests.post", side_effect=mocked_requests_post)
+    def test_post_410(self, mock_post):
+        response = post("url_410", {"param": "valid"}, 201)
+        self.assertEqual(
+            response.err, {"url": "url_410", "error": "Unexpected Status Code"}
+        )
+        self.assertEqual(response.is_ok(), False)
+
+    # wrong post parameters
+    @mock.patch("requests.post", side_effect=mocked_requests_post)
+    def test_post_with_wrong_params(self, mock_post):
+        response = post("url_201", {"param": "invalid"}, 201)
+        self.assertEqual(response.err, {"url": "url_201", "error": "403 Forbidden"})
+        self.assertEqual(response.is_ok(), False)
+
+    # wrong post parameters
+    @mock.patch("requests.post", side_effect=mocked_requests_post)
+    def test_post_wait_wrong_sutatuscode(self, mock_post):
+        response = post("url_201", {"param": "valid"}, 202)
+        self.assertEqual(
+            response.err, {"url": "url_201", "error": "Unexpected Status Code"}
+        )
+        self.assertEqual(response.is_ok(), False)
+
+    # -------------------- create peer --------------------
+    # create peer success
+    @mock.patch("requests.post", side_effect=mocked_requests_post)
+    def test_create_peer_success(self, mock_post):
+        response = create_peer(
+            "url_create_peer_success", "key", "domain", PeerId("peerid"), True
+        )
+        self.assertEqual(
+            response.json(),
+            {"turn": True, "domain": "domain", "peer_id": "peerid", "key": "key"},
+        )
+        self.assertEqual(response.is_ok(), True)
+
+    # create peer gets invalid response
+    @mock.patch("requests.post", side_effect=mocked_requests_post)
+    def test_create_peer_fail(self, mock_post):
+        response = create_peer(
+            "url_create_peer_fail", "key", "domain", PeerId("peerid"), True
+        )
+        self.assertEqual(
+            response.err,
+            {"url": "url_create_peer_fail/peers", "error": "404 Not Found"},
+        )
+        self.assertEqual(response.is_ok(), False)
+
+    # create peer fail due to http error
+    def test_create_peer_no_server(self):
+        response = create_peer(
+            "http://url_create_peer_fail", "key", "domain", PeerId("peerid"), True
+        )
+        self.assertEqual(
+            isinstance(response.err, requests.exceptions.RequestException), True
+        )
+        self.assertEqual(response.is_ok(), False)
 
 
 if __name__ == "__main__":
     import rostest
 
-    rostest.rosrun(PKG, "skyway", TestPeer)
+    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+    rostest.rosrun(PKG, "test-skyway", TestPostResp)
