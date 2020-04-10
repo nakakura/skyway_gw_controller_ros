3da5042ee261328aa638c8e162db74caf113a8e7
Toshiya Nakakura <nakakura@me.com>
2020-04-10 17:09:55 +0900

    recv data connection

--- scripts/skyway.py
+++ scripts/skyway.py
@@ -2,9 +2,11 @@
 # license removed for brevity
 import os
 import rospy
+import sys
 from concurrent import futures
 import time
-from multiprocessing import Queue
+import multiprocessing
+import Queue
 import random
 
 from api.peer import PeerInfo, create_peer, delete_peer, listen_event
@@ -12,12 +14,12 @@ import const
 
 is_running = True
 
-
 # This method is responsible for deleting peer object when rospy close
 def runloop(peer_info):
     # type: (PeerInfo) -> None
     global is_running
     # wait for ros termination
+    i = 0
     rate = rospy.Rate(10)  # 10hz
     while is_running:
         rate.sleep()
@@ -27,8 +29,8 @@ def runloop(peer_info):
         raise resp.err()
 
 
-def listen_peer_events(peer_info):
-    # type: (PeerInfo) -> None
+def listen_peer_events(peer_info, media_event_queue, data_event_queue):
+    # type: (PeerInfo, multiprocessing.Queue, multiprocessing.Queue) -> None
     global is_running
     rate = rospy.Rate(10)  # 10hz
     # polling while ros is running
@@ -39,14 +41,54 @@ def listen_peer_events(peer_info):
             err = resp.err()
             # polling timeout
             if "status" in err and err["status"] == 408:
-                rospy.logerr("continue")
                 continue
             else:
                 raise err
 
         json = resp.json()
-        if "event" in json and json["event"] == "CLOSE":
+        if not "event" in json:
+            continue
+        elif json["event"] == "CLOSE":
             break
+        elif json["event"] == "OPEN":
+            params = json["params"]
+            peer_info = PeerInfo(params["peer_id"], params["token"])
+        elif json["event"] == "CONNECTION":
+            params = json["data_params"]
+            params["type"] = "CONNECTION"
+            data_event_queue.put(params)
+
+
+def data(queue):
+    # type: (multiprocessing.Queue) -> None
+    global is_running
+    rate = rospy.Rate(10)  # 10hz
+    # polling while ros is running
+    while is_running:
+        try:
+            message = queue.get(timeout=0.2)
+            if message["type"] == "CONNECTION":
+                rospy.logerr("data: {}".format(message))
+        except Queue.Empty as e:
+            continue
+        except Exception as e:
+            rospy.logerr(sys.exc_info())
+            rospy.logerr("We lacked patience and got a multiprocessing.TimeoutError")
+
+
+def media(queue):
+    # type: (multiprocessing.Queue) -> None
+    global is_running
+    rate = rospy.Rate(10)  # 10hz
+    # polling while ros is running
+    while is_running:
+        try:
+            message = queue.get(timeout=0.2)
+        except Queue.Empty as e:
+            continue
+        except Exception as e:
+            rospy.logerr(sys.exc_info())
+            rospy.logerr("We lacked patience and got a multiprocessing.TimeoutError")
 
 
 def skyway():
@@ -70,17 +112,26 @@ def skyway():
 
     rate = rospy.Rate(10)  # 10hz
     with futures.ThreadPoolExecutor(max_workers=8) as executor:
+        media_event_queue = multiprocessing.Queue()
+        data_event_queue = multiprocessing.Queue()
         # delete peer object when rospy close
-        f1 = executor.submit(runloop, peer_info)
+        closing_future = executor.submit(runloop, peer_info)
         # poll events from PeerObject
-        f2 = executor.submit(listen_peer_events, peer_info)
+        peer_event_future = executor.submit(
+            listen_peer_events, peer_info, media_event_queue, data_event_queue
+        )
+
+        data_future = executor.submit(data, data_event_queue)
+        media_future = executor.submit(media, media_event_queue)
         # keep checking rospy status
         while not rospy.is_shutdown():
             rate.sleep()
         # exit rospy
         is_running = False
-        f1.result()
-        f2.result()
+        closing_future.result()
+        peer_event_future.result()
+        data_future.result()
+        media_future.result()
 
     # notify and wait just in case
     rospy.signal_shutdown("finish")
